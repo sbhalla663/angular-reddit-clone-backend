9884929fe2afc1cfa8a29aec61465eece4798089
sbhalla663 <sbhalla663@gmail.com>
2021-05-16 13:00:32 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/exceptions/SpringRedditException.java
@@ -0,0 +1,8 @@
+package com.example.demo.exceptions;
+
+
+public class SpringRedditException extends RuntimeException {
+    public SpringRedditException(String exMessage) {
+        super(exMessage);
+    }
+}
